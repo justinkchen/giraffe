38bf8f21cc95438e2a3c6fd5f938d87c9573e33b
Kurt Horimoto <kkhorimoto@gmail.com>
2013-03-04 13:06:05 -0800

    Introduced table view + started working on a separate cell class to use.

--- ProjectGiraffe/inc/ProjectGiraffeTab1.h
+++ ProjectGiraffe/inc/ProjectGiraffeTab1.h
@@ -3,11 +3,14 @@
 
 #include <FBase.h>
 #include <FUi.h>
+#include "GraffitiCell.h"
 
 class ProjectGiraffeTab1
 	: public Tizen::Ui::Controls::Panel
 	, public Tizen::Ui::Scenes::ISceneEventListener
-{
+	, public Tizen::Ui::Controls::ITableViewItemProvider
+	, public Tizen::Ui::Controls::ITableViewItemEventListener
+	{
 public:
 	ProjectGiraffeTab1(void);
 	virtual ~ProjectGiraffeTab1(void);
@@ -21,6 +24,26 @@ public:
 	virtual void OnSceneDeactivated(const Tizen::Ui::Scenes::SceneId& currentSceneId,
 									const Tizen::Ui::Scenes::SceneId& nextSceneId);
 
+    // ITableViewItemEventListener
+    virtual void OnTableViewItemStateChanged(Tizen::Ui::Controls::TableView& tableView, int itemIndex, Tizen::Ui::Controls::TableViewItem* pItem, Tizen::Ui::Controls::TableViewItemStatus status);
+    virtual void OnTableViewContextItemActivationStateChanged(Tizen::Ui::Controls::TableView& tableView, int itemIndex, Tizen::Ui::Controls::TableViewContextItem* pContextItem, bool activated);
+    virtual void OnTableViewItemReordered(Tizen::Ui::Controls::TableView& tableView, int itemIndexFrom, int itemIndexTo);
+
+    // ITableViewItemProvider
+    virtual int GetItemCount(void);
+    virtual Tizen::Ui::Controls::TableViewItem* CreateItem(int itemIndex, int itemWidth);
+    virtual bool DeleteItem(int itemIndex, Tizen::Ui::Controls::TableViewItem* pItem);
+    virtual void UpdateItem(int itemIndex, Tizen::Ui::Controls::TableViewItem* pItem);
+    virtual int GetDefaultItemHeight(void);
+
+private:
+    Tizen::Ui::Controls::TableView* tableView;
+    Tizen::Ui::Controls::TableViewContextItem* tableViewContextItem;
+
+    unsigned int itemCount;
+    //GraffitiCell **itemCells;
+    Tizen::Base::String *itemTitles;
+    virtual void updateItems();
 };
 
 #endif // _PROJECTGIRAFFE_TAB1_H_
--- ProjectGiraffe/src/ProjectGiraffeTab1.cpp
+++ ProjectGiraffe/src/ProjectGiraffeTab1.cpp
@@ -1,5 +1,9 @@
 #include "ProjectGiraffeTab1.h"
 
+using namespace Tizen::App;
+using namespace Tizen::Base;
+using namespace Tizen::Media;
+
 using namespace Tizen::Graphics;
 using namespace Tizen::Ui;
 using namespace Tizen::Ui::Controls;
@@ -25,6 +29,39 @@ ProjectGiraffeTab1::Initialize(void)
 	return true;
 }
 
+#define kDebugUseDummyItems 1
+
+void
+ProjectGiraffeTab1::updateItems()
+{
+#if kDebugUseDummyItems
+    itemCount = 50;
+    /*
+    itemCells = new GraffitiCell*[itemCount];
+    for (int i = 0; i < itemCount; i++) {
+    	GraffitiCell *cell = new GraffitiCell();
+    	//cell->Initialize();
+    	cell->setUsername(L"Username");
+    	cell->setDistance(i);
+    	cell->setText(L"Graffiti!");
+    	itemCells[i] = cell;
+    }
+    */
+
+
+    itemTitles = new String[itemCount];
+
+    for (int i = 0; i < itemCount; i++) {
+        String title;
+        title.Format(30, L"Item %d", i);
+        itemTitles[i] = title;
+    }
+
+#else
+    // Kick off http request for items based on location.
+#endif
+}
+
 result
 ProjectGiraffeTab1::OnInitializing(void)
 {
@@ -32,6 +69,7 @@ ProjectGiraffeTab1::OnInitializing(void)
 
 	// Layout setting
 	const Form* pForm = dynamic_cast<Form*>(GetParent());
+    Rectangle formBounds = pForm->GetClientAreaBounds();
 	RelativeLayout* pRelativeLayout = dynamic_cast<RelativeLayout*>(pForm->GetLandscapeLayoutN());
 	pRelativeLayout->SetHorizontalFitPolicy(*this, FIT_POLICY_PARENT);
 	pRelativeLayout->SetVerticalFitPolicy(*this, FIT_POLICY_PARENT);
@@ -40,6 +78,18 @@ ProjectGiraffeTab1::OnInitializing(void)
 	pRelativeLayout->SetHorizontalFitPolicy(*this, FIT_POLICY_PARENT);
 	pRelativeLayout->SetVerticalFitPolicy(*this, FIT_POLICY_PARENT);
 	delete pRelativeLayout;
+
+    // Create tableView
+    tableView = new TableView();
+    tableView->Construct(Rectangle(0,0,formBounds.width, formBounds.height), \
+                         true, TABLE_VIEW_SCROLL_BAR_STYLE_FADE_OUT);
+    tableView->SetItemProvider(this);
+    tableView->AddTableViewItemEventListener(*this);
+    AddControl(*tableView);
+
+    // Create tableViewContextItem
+    tableViewContextItem = new TableViewContextItem();
+    tableViewContextItem->Construct(Dimension(720,100));
 
 	return r;
 }
@@ -58,8 +108,9 @@ void
 ProjectGiraffeTab1::OnSceneActivatedN(const Tizen::Ui::Scenes::SceneId& previousSceneId,
 								const Tizen::Ui::Scenes::SceneId& currentSceneId, Tizen::Base::Collection::IList* pArgs)
 {
-	// TODO:
-	// Add your scene activate code here
+    // Populate item source array
+    updateItems();
+
 	AppLog("OnSceneActivatedN");
 }
 
@@ -71,3 +122,54 @@ ProjectGiraffeTab1::OnSceneDeactivated(const Tizen::Ui::Scenes::SceneId& current
 	// Add your scene deactivate code here
 	AppLog("OnSceneDeactivated");
 }
+
+// ITableViewItemEventListener
+void ProjectGiraffeTab1::OnTableViewItemStateChanged(Tizen::Ui::Controls::TableView& tableView, int itemIndex, Tizen::Ui::Controls::TableViewItem* pItem, Tizen::Ui::Controls::TableViewItemStatus status)
+{
+}
+    
+void ProjectGiraffeTab1::OnTableViewContextItemActivationStateChanged(Tizen::Ui::Controls::TableView& tableView, int itemIndex, Tizen::Ui::Controls::TableViewContextItem* pContextItem, bool activated)
+{
+}
+    
+void ProjectGiraffeTab1::OnTableViewItemReordered(Tizen::Ui::Controls::TableView& tableView, int itemIndexFrom, int itemIndexTo)
+{
+}
+    
+// ITableViewItemProvider
+int ProjectGiraffeTab1::GetItemCount(void)
+{
+    return itemCount;
+}
+
+TableViewItem* ProjectGiraffeTab1::CreateItem(int itemIndex, int itemWidth)
+{
+    TableViewItem *item = new TableViewItem();
+    item->Construct(Dimension(itemWidth, GetDefaultItemHeight()),
+                    TABLE_VIEW_ANNEX_STYLE_NORMAL);
+    //item->AddControl(*itemCells[itemIndex]);
+
+    Label*label = new Label();
+    label->Construct(Rectangle(0,0,itemWidth,item->GetBounds().height), itemTitles[itemIndex]);
+    item->AddControl(*label);
+    item->SetContextItem(tableViewContextItem);
+
+
+    return item;
+}
+    
+bool ProjectGiraffeTab1::DeleteItem(int itemIndex, Tizen::Ui::Controls::TableViewItem* pItem)
+{
+    delete pItem;
+    return true;
+}
+    
+void ProjectGiraffeTab1::UpdateItem(int itemIndex, Tizen::Ui::Controls::TableViewItem* pItem)
+{
+}
+    
+int ProjectGiraffeTab1::GetDefaultItemHeight(void)
+{
+    return 150;
+}
+
