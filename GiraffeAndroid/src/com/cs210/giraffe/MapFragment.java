8065d31b222042e7d7b4f561560e2dd52a6bbde9
Justin K. Chen <justinkchen@stanford.edu>
2013-04-04 22:52:17 -0700

    setting up template files to put in features

---
+++ GiraffeAndroid/src/com/cs210/giraffe/MapFragment.java
@@ -0,0 +1,10 @@
+package com.cs210.giraffe;
+
+import android.support.v4.app.Fragment;
+
+public class MapFragment extends Fragment {
+
+	public MapFragment() {
+		// TODO Auto-generated constructor stub
+	}
+}
---
+++ GiraffeAndroid/src/com/cs210/giraffe/SettingsActivity.java
@@ -0,0 +1,11 @@
+package com.cs210.giraffe;
+
+import android.app.Activity;
+
+public class SettingsActivity extends Activity {
+
+	public SettingsActivity() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
