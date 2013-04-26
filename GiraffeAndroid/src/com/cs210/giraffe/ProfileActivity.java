471730abf2338052ab48e93f9bfc460332afb1ac
Johan Lindell <johanlindell@stanford.edu>
2013-04-26 01:32:06 -0700

    More user

--- GiraffeAndroid/src/com/cs210/giraffe/ProfileActivity.java
+++ GiraffeAndroid/src/com/cs210/giraffe/ProfileActivity.java
@@ -2,9 +2,13 @@ package com.cs210.giraffe;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class ProfileActivity extends Activity {
 
+	private ProfileFragment _pf;
+	
 	public ProfileActivity() {
 		// TODO Auto-generated constructor stub
 	}
@@ -12,9 +16,14 @@ public class ProfileActivity extends Activity {
 	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        _pf = new ProfileFragment();
         getFragmentManager().beginTransaction()
-    		.replace(android.R.id.content, new ProfileFragment())
+    		.replace(android.R.id.content, _pf)
     		.commit();
         getActionBar().setDisplayHomeAsUpEnabled(true);
     }
+	
+	public void changePassword(View v) {
+		_pf.setUsername("Clicked");
+	}
 }
--- GiraffeAndroid/src/com/cs210/giraffe/ProfileFragment.java
+++ GiraffeAndroid/src/com/cs210/giraffe/ProfileFragment.java
@@ -1,9 +1,20 @@
 package com.cs210.giraffe;
 
+
 import android.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class ProfileFragment extends Fragment {
+	
+	private TextView _userText;
+	private ListView _userPosts;
+	
 	
 	public ProfileFragment () {
 		
@@ -14,4 +25,21 @@ public class ProfileFragment extends Fragment {
     	super.onCreate(savedInstanceState);
 
     }
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View rootView = inflater.inflate(R.layout.fragment_profile,
+				container, false);
+		
+		_userText = (TextView) rootView.findViewById(R.id.userTextView);
+		_userPosts = (ListView) rootView.findViewById(R.id.userPostListView);
+		return rootView;
+	}
+	
+	public void setUsername(String s) {
+		_userText.setText(s);
+	}
+	
+	
 }
