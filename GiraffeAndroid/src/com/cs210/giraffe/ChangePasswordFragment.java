c87397fe30a07f67843c0c38af5a91d21364c5dd
Johan Lindell <johanlindell@stanford.edu>
2013-04-28 14:55:27 -0700

    Fixed nullpointerexception

--- GiraffeAndroid/src/com/cs210/giraffe/ChangePasswordFragment.java
+++ GiraffeAndroid/src/com/cs210/giraffe/ChangePasswordFragment.java
@@ -6,27 +6,49 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class ChangePasswordFragment extends DialogFragment{
+	
+	private String _oldPassword;
+	private String _newPassword1;
+	private String _newPassword2;
+	private TextView _errorMessage;
 	
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         // Use the Builder class for convenient dialog construction
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         LayoutInflater inflater = getActivity().getLayoutInflater();
-        builder.setView(inflater.inflate(R.layout.password_dialog, null))
-	        .setPositiveButton(R.string.submit, new DialogInterface.OnClickListener() {
-	            @Override
-	            public void onClick(DialogInterface dialog, int id) {
-	                // sign in the user ...
-	            }
-	        })
-	        .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
-	            public void onClick(DialogInterface dialog, int id) {
-	            	ChangePasswordFragment.this.getDialog().cancel();
-	            }
-	            });
+       
+        
+        
+        builder.setView(inflater.inflate(R.layout.password_dialog, null)).setTitle("Change Password");
         return builder.create();
     }
+    
+    public void submitChangePassword() {
+    	_errorMessage = (TextView) getDialog().findViewById(R.id.errormessage);
+    	_oldPassword = ((EditText) getDialog().findViewById(R.id.oldpassword)).getText().toString();
+    	_newPassword1 = ((EditText) getDialog().findViewById(R.id.newpassword1)).getText().toString();
+    	_newPassword2 = ((EditText) getDialog().findViewById(R.id.newpassword2)).getText().toString();
+    	
+    	if (_oldPassword.isEmpty() || _newPassword1.isEmpty() || _newPassword2.isEmpty())
+    		_errorMessage.setText("	You have to fill out all of the fields");	
+    	else if (!_newPassword1.equals(_newPassword2))
+    		_errorMessage.setText("	New password doesn't match");	
+    	else {
+    		changePassword(_oldPassword, _newPassword1);
+    		getDialog().cancel();
+    	}
+    		
+    }
+    
+    private void changePassword(String oldPassword, String newPassword) {
+    	//TODO: Change password
+    }
 
+    
 }
--- GiraffeAndroid/src/com/cs210/giraffe/ProfileActivity.java
+++ GiraffeAndroid/src/com/cs210/giraffe/ProfileActivity.java
@@ -1,15 +1,19 @@
 package com.cs210.giraffe;
 
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 public class ProfileActivity extends FragmentActivity {
 
 	private ProfileFragment _pf;
+	private ChangePasswordFragment _df;
+	TextView _passwordErrorMessage;
 	
 	public ProfileActivity() {
 		// TODO Auto-generated constructor stub
@@ -27,7 +31,17 @@ public class ProfileActivity extends FragmentActivity {
     }
 	
 	public void changePassword(View v) {
-		DialogFragment newFragment = new ChangePasswordFragment();
-	    newFragment.show(getSupportFragmentManager(), "changePassword");
+		_df = new ChangePasswordFragment();
+	    _df.show(getSupportFragmentManager(), "changePassword");
 	}
+	
+	public void cancelPasswordDialog(View v) {
+    	_df.dismiss();
+    }
+	
+	public void submitPasswordDialog(View v) {
+		_df.submitChangePassword();
+	}
+	
+	
 }
