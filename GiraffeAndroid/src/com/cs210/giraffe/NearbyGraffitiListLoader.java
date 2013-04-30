35cd04e88b0c369bb5d8a8fc6e08ded139e80f55
Justin K. Chen <justinkchen@stanford.edu>
2013-04-30 01:34:22 -0700

    fixed to make dynamic url

--- GiraffeAndroid/src/com/cs210/giraffe/NearbyGraffitiListLoader.java
+++ GiraffeAndroid/src/com/cs210/giraffe/NearbyGraffitiListLoader.java
@@ -33,8 +33,8 @@ public class NearbyGraffitiListLoader extends AsyncTaskLoader<List<Graffiti>> {
 		URL url = null;
 		Location currLocation = MainActivity.getGiraffeLocationListener().getCurrentLocation();
 		try {
-			//url = new URL("http://ec2-54-243-69-6.compute-1.amazonaws.com/graffiti/nearby" + "?latitude=" + currLocation.getLatitude() + "&longitude=" + currLocation.getLongitude());
-			url = new URL("http://ec2-54-243-69-6.compute-1.amazonaws.com/graffiti/nearby?latitude=37.4280040&longitude=-122.1706350");
+			url = new URL("http://ec2-54-243-69-6.compute-1.amazonaws.com/graffiti/nearby" + "?latitude=" + currLocation.getLatitude() + "&longitude=" + currLocation.getLongitude());
+			//url = new URL("http://ec2-54-243-69-6.compute-1.amazonaws.com/graffiti/nearby?latitude=37.4280040&longitude=-122.1706350");
 		} 
 		catch (MalformedURLException e1) {
 			// TODO Auto-generated catch block
