993f977b342e449a53a1afb12bdeb762e9b6d132
Johan Lindell <johanlindell@stanford.edu>
2013-05-04 18:13:53 -0700

    Started work on changing profile pic

--- GiraffeAndroid/src/com/cs210/giraffe/SettingsFragment.java
+++ GiraffeAndroid/src/com/cs210/giraffe/SettingsFragment.java
@@ -1,40 +1,51 @@
 package com.cs210.giraffe;
 
-import android.app.DialogFragment;
 import android.os.Bundle;
-import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.preference.Preference.OnPreferenceClickListener;
 import android.util.Log;
 
 public class SettingsFragment extends PreferenceFragment {
-	
+
 	public ChangePasswordFragment df;
-	
+
 	public SettingsFragment() {
 		// TODO Auto-generated constructor stub
 	}
-	
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-    	super.onCreate(savedInstanceState);
 
-        // Load the preferences from an XML resource
-        addPreferencesFromResource(R.xml.preferences);
-        
-        Preference changePasswordPreference =  findPreference("change_password");
-        changePasswordPreference.setOnPreferenceClickListener(new myOnPreferenceClickListener());
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
-   
-    private class myOnPreferenceClickListener implements OnPreferenceClickListener {
-    	
-    	public boolean onPreferenceClick(Preference preference) {
-        	Log.i("Johan", "Click");
-        	df = new ChangePasswordFragment();
-    	    df.show(getFragmentManager(), "changePassword");
-            return true;
-        }
-    }
+		// Load the preferences from an XML resource
+		addPreferencesFromResource(R.xml.preferences);
+
+		Preference changePasswordPreference = findPreference("change_password");
+		changePasswordPreference
+				.setOnPreferenceClickListener(new passwordOnPreferenceClickListener());
+
+		Preference changeProfileImagePreference = findPreference("change_profile_image");
+		changeProfileImagePreference
+				.setOnPreferenceClickListener(new profileImageOnPreferenceClickListener());
+	}
+
+	private class passwordOnPreferenceClickListener implements
+			OnPreferenceClickListener {
+
+		public boolean onPreferenceClick(Preference preference) {
+			df = new ChangePasswordFragment();
+			df.show(getFragmentManager(), "changePassword");
+			return true;
+		}
+	}
+
+	private class profileImageOnPreferenceClickListener implements
+			OnPreferenceClickListener {
+
+		public boolean onPreferenceClick(Preference preference) {
+			
+			return true;
+		}
+	}
 }
