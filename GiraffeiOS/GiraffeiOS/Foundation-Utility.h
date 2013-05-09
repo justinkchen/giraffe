e999fd78e7994ea1cc59e9d34a930619821b724c
Kurt Horimoto <kkhorimoto@gmail.com>
2013-05-08 22:10:07 -0700

    Created Foundation-Utility category.  Added files to Xcode project.

---
+++ GiraffeiOS/GiraffeiOS/Foundation-Utility.h
@@ -0,0 +1,15 @@
+//
+//  Foundation-Utility.h
+//  GiraffeiOS
+//
+//  Created by Kurtis Horimoto on 5/8/13.
+//  Copyright (c) 2013 Unsamsung Heroes. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (Utility)
+
+- (id)ifIsKindOfClass:(Class)aClass;
+
+@end
