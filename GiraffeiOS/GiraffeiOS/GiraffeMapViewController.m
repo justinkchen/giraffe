48b8a7cf09e94e39ad3b7a3503c3952209a08b33
Kurt Horimoto <kkhorimoto@gmail.com>
2013-05-08 23:31:21 -0700

    Insert heatmap webview.

--- GiraffeiOS/GiraffeiOS/GiraffeMapViewController.m
+++ GiraffeiOS/GiraffeiOS/GiraffeMapViewController.m
@@ -7,23 +7,42 @@
 //
 
 #import "GiraffeMapViewController.h"
+#import "LocationManager.h"
 
-@interface GiraffeMapViewController ()
+@interface GiraffeMapViewController () <UIWebViewDelegate>
+
+@property (nonatomic, retain) UIWebView *webView;
 
 @end
 
 @implementation GiraffeMapViewController
 
+#pragma mark - View Lifecycle
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    self.webView = [[UIWebView alloc] initWithFrame:self.view.bounds];
+    self.webView.delegate = self;
+    [self.view addSubview:self.webView];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+NSString *const kHeatmapUrlFormat = @"http://ec2-54-243-69-6.compute-1.amazonaws.com/graffitimap.html?latitude=%f&longitude=%f";
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    
+    NSString *urlString = [NSString stringWithFormat:kHeatmapUrlFormat, [LocationManager sharedInstance].latitude, [LocationManager sharedInstance].longitude];
+    NSURLRequest *request = [[NSURLRequest alloc] initWithURL:[NSURL URLWithString:urlString]];
+    [self.webView loadRequest:request];
 }
 
 @end
