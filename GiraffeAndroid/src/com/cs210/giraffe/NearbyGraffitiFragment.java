662be3011d2423806af6e1e80633f00e3a9a1595
Justin K. Chen <justinkchen@stanford.edu>
2013-05-09 18:42:13 -0700

    fixed a crash on nearbylist

--- GiraffeAndroid/src/com/cs210/giraffe/NearbyGraffitiFragment.java
+++ GiraffeAndroid/src/com/cs210/giraffe/NearbyGraffitiFragment.java
@@ -4,6 +4,8 @@ import com.google.android.gms.maps.SupportMapFragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -33,5 +35,20 @@ public class NearbyGraffitiFragment extends Fragment {
 		});
 		
 		return rootView;
+	}
+	
+	@Override
+	public void onDestroyView() {
+		super.onDestroyView();
+		if(!getActivity().isFinishing()){
+			Log.w("NearbyListFragment", "Destroying View");
+			NearbyListFragment f = (NearbyListFragment) this.getActivity()
+					.getSupportFragmentManager()
+					.findFragmentById(R.id.nearbyListFragment);
+			if (f != null) {
+				FragmentTransaction ft = this.getActivity().getSupportFragmentManager().beginTransaction();
+				ft.remove(f).commit();
+			}
+		}
 	}
 }
