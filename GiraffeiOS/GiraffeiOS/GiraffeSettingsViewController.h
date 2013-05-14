ddda478a66113ccc121c860d9bd35e0f9d1b2c07
Bryan Cheng <bryan.b.cheng@gmail.com>
2013-05-14 15:20:44 -0700

    added settings page again...

--- GiraffeiOS/GiraffeiOS/GiraffeSettingsViewController.h
+++ GiraffeiOS/GiraffeiOS/GiraffeSettingsViewController.h
@@ -7,6 +7,8 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "User.h"
+#import "GiraffeClient.h"
 
 @interface GiraffeSettingsViewController : UIViewController
 
--- GiraffeiOS/GiraffeiOS/GiraffeSettingsViewController.m
+++ GiraffeiOS/GiraffeiOS/GiraffeSettingsViewController.m
@@ -10,9 +10,21 @@
 
 @interface GiraffeSettingsViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *usernameField;
+@property (weak, nonatomic) IBOutlet UITextField *emailField;
+@property (weak, nonatomic) IBOutlet UITextField *oldPasswordField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordField;
+@property (weak, nonatomic) IBOutlet UITextField *confirmPasswordField;
+
 @end
 
 @implementation GiraffeSettingsViewController
+
+@synthesize usernameField = _usernameField;
+@synthesize emailField = _emailField;
+@synthesize oldPasswordField = _oldPasswordField;
+@synthesize passwordField = _passwordField;
+@synthesize confirmPasswordField = _confirmPasswordField;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -34,5 +46,22 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (IBAction)updateAccount:(UIButton *)sender {
+    
+}
+
+- (IBAction)changePassword:(UIButton *)sender {
+    
+}
+
+- (IBAction)logout:(UIButton *)sender {
+    [[GiraffeClient sharedClient] beginUserLogoutPostWithUser:[User currentUser] success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        [[User currentUser] logout];
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        // print error
+    }];
+}
+
 
 @end
